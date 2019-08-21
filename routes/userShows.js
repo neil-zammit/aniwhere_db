d927a017b57ecd022b2a7765704a2a2cf9b4a1d0
Neil Zammit <nz3001@gmail.com>
2019-08-21 15:13:26 +0200

    Add functionality for users to remove shows from their personal collection

--- routes/userShows.js
+++ routes/userShows.js
@@ -27,7 +27,7 @@ router.get('/myshows', ensureAuthenticated, (req, res) =>
     .catch(err => console.log(err))
 );
 
-// Form Route
+// Add Show Form Route
 router.get('/addmyshows', ensureAuthenticated, (req, res) =>
   res.render('addmyshows', {
     layout: false
@@ -82,4 +82,60 @@ router.post('/myshows', ensureAuthenticated, (req, res) => {
   }
 });
 
+// Remove Show Form Route
+router.get('/removemyshows', ensureAuthenticated, (req, res) =>
+  res.render('removemyshows', {
+    layout: false
+  })
+);
+
+// Delete Show from MyShows Route
+router.post('/removemyshows', ensureAuthenticated, (req, res) => {
+  // Retrieve data from form
+  let { title, image } = req.body;
+  // Retrieve current user
+  const user_id = req.user;
+  // Create array to store errors for form validation
+  let errors = [];
+
+  // Form Validation
+  if (!title) {
+    errors.push({ text: 'Add show title ' });
+  }
+  if (!image) {
+    errors.push({ text: 'Add image location ' });
+  }
+
+  // If error in errors array, display error
+  if (errors.length > 0) {
+    res.render('addmyshows', {
+      errors,
+      title,
+      image,
+      layout: false
+    });
+  } else {
+    // Retrieve show data
+    Shows.findOne({
+      raw: true,
+      // find show from db which matches the user's input
+      where: { title: req.body.title }
+    })
+      .then(shows => {
+        const show_id = shows.id;
+        // Remove from table
+        UserShows.destroy({
+          where: {
+            user_id,
+            show_id
+          }
+        })
+          .then(shows => res.redirect('/myshows'))
+          .catch(err => console.log(err));
+      })
+      .catch(err => {
+        console.log('Error finding shows : ', err);
+      });
+  }
+});
 module.exports = router;
